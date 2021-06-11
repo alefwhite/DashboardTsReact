365b0137732b0aa67533c50b0ec6842fb42dd736
alef.silva <alef.silva@fcamara.com.br>
2021-06-11 15:52:49 -0300

    Criação do tema light e dark, estilizando MainHeader e Incluindo emojis dinamicos

--- frontend/src/components/MainHeader/index.tsx
+++ frontend/src/components/MainHeader/index.tsx
@@ -1,10 +1,27 @@
-import React from "react";
-import { Container } from './styles';
+import React, { useMemo } from "react";
+import emojis from '../../utils/emojis';
+
+import Toggle from '../Toggle';
+
+import { Container, Profile, Welcome, UserName } from './styles';
 
 const MainHeader: React.FC = () => {
+
+    const emoji = useMemo(() => {
+        const indice = Math.floor(Math.random() * emojis.length);
+
+        return emojis[indice];
+
+    }, []);
+    
     return (
         <Container>
-            <h1>MainHeader</h1>
+            <Toggle/>
+
+            <Profile>
+                <Welcome>Olá, {emoji}</Welcome>
+                <UserName>Alef Cauê</UserName>
+            </Profile>
         </Container>
     );
 }
